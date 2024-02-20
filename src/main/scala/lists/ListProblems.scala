50910b764211603e1b45c5fd40f00d36654fb426
srinaththota <srinath.thota@gmail.com>
2024-02-20 16:17:17 +0100

    added gitignore

--- src/main/scala/lists/ListProblems.scala
+++ src/main/scala/lists/ListProblems.scala
@@ -1,5 +1,16 @@
 package lists
+sealed abstract class RList[+T]{
+  def head: T
+  def tail: RList[T]
+  def isEmpty: Boolean
+}
 
-object ListProblems {
+case object RNil extends RList[Nothing] {
+  override def head:Nothing = throw new NoSuchElementException
+  override def tail: RList[Nothing] = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+}
+object ListProblems extends App {
 
+  println("checking")
 }
